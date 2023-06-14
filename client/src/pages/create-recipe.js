9a5a6883f83465ab22f98081d3405582d914eac7
AhmadSamra <a.abusamra@outlook.com>
2023-06-15 01:07:25 +0200

    mern recipe

--- client/src/pages/create-recipe.js
+++ client/src/pages/create-recipe.js
@@ -1,3 +1,81 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useGetUserID } from "../hooks/useGetUserID";
+import { useNavigate } from 'react-router-dom';
+
 export const CreateRecipe = () => {
-    return <div>CreateRecipe</div>
+  const userID = useGetUserID();
+  const [recipe, setRecipe] = useState({
+    name: "",
+    ingredients: [],
+    instructions: "",
+    imageUrl: "",
+    cookingTime: 0,
+    userOwner: userID,
+  });
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setRecipe({ ...recipe, [name]: value });
+  };
+
+  const handleIngredientChange = (e, index) => {
+    const { value } = e.target;
+    const ingredients = recipe.ingredients;
+    ingredients[index] = value;
+    setRecipe({ ...recipe, ingredients });
+  };
+
+  const addIngredient = () => {
+    setRecipe({ ...recipe, ingredients: [...recipe.ingredients, ""]});
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:3002/recipes", recipe);
+      alert("Recipe created successfully");
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  return (
+    <div className="create-recipe">
+      <h2>Create Recipe</h2>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" name="name" onChange={handleChange} />
+        <label htmlFor="ingredients">Ingredients</label>
+        {recipe.ingredients.map((ingredient, index) => (
+          <input
+            key={index}
+            type="text"
+            name="ingredients"
+            value={ingredient}
+            onChange={(e) => handleIngredientChange(e, index)}
+          />
+        ))}
+        <button onClick={addIngredient} type="button">
+          Add Ingredient
+        </button>
+        <label htmlFor="instructions">Instructions</label>
+        <textarea
+          name="instructions"
+          id="instructions"
+          onChange={handleChange}
+        ></textarea>
+        <label htmlFor="imageUrl">Image URL</label>
+        <input type="text" id="imageUrl" name="imageUrl" onChange={handleChange}/>
+        <label htmlFor="cookingTime">Cooking Time</label>
+        <input
+          type="number"
+          id="cookingTime"
+          name="cookingTime"
+          onChange={handleChange}
+        />
+        <button type="submit">Create Recipe</button>
+      </form>
+    </div>
+  );
 };
--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,3 +1,48 @@
+import React, {useEffect,useState} from 'react';
+import axios from 'axios';
+import { useGetUserID } from './../hooks/useGetUserID';
+
 export const Home = () => {
-    return <div>Home</div>
+    const [recipes, setRecipes] = useState([]);
+    const userID = useGetUserID();
+    useEffect(() => {
+        const fetchRecipes = async () => {
+            try{
+                const response = await axios.get('http://localhost:3002/recipes');
+                setRecipes(response.data);
+                console.log(response.data)
+            }catch(err){
+                console.log(err)
+            }
+        }
+        fetchRecipes();
+    },[]);
+
+    const saveRecipe = async (recipeID) => {
+        try{
+            const response = await axios.put('http://localhost:3002/recipes',{recipeID,userID});
+            console.log(response)
+        }catch(err){
+            console.log(err)
+        }
+    }
+
+    return <div>
+        <h1>Recipes</h1>
+        <ul>
+            {recipes.map((recipe)=>(
+                <li key={recipe._id}>
+                 <div>
+                    <h2>{recipe.name}</h2>
+                    <button onClick={() => saveRecipe(recipe._id)}>Save</button>
+                 </div>
+                 <div className="instructions">
+                    <p>{recipe.instructions}</p>
+                 </div>
+                 <img src={recipe.imageUrl} alt={recipe.name} />
+                  <p>Cooking Time: {recipe.cookingTime} (minutes)</p>  
+                </li>
+            ))}
+        </ul>
+    </div>
 };
